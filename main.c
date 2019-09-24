99737bf8fbb020b08d5aea9801568ec04ccbd8e7
littlebird <55723225+littlebirds2019@users.noreply.github.com>
2019-09-24 10:49:03 +0800

    c/C语言告白代码

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	char love[20];
+	int loveme;
+	printf("请输入你最爱的人的名字和属于她的玫瑰密码：\n");
+	gets(love);
+	while(1)
+	{
+	scanf("%d",&loveme); 
+	if(loveme==520)
+	{
+	printf("哈哈哈哈被你答对了，送你18朵玫瑰，要永远年轻哦哦\n");
+   	printf("  \\|/ \\|/ \\|/ \\|/ \\|/ \\|/ \n");
+   	printf("  - - - - - - - - - - - -  \n");
+	printf("   |   |   |   |   |   | \n");
+	printf("   |   |   |   |   |   | \n");
+	printf("\n"); 
+
+	printf("  \\|/ \\|/ \\|/ \\|/ \\|/ \\|/ \n");
+	printf("  - - - - - - - - - - - -  \n");
+    printf("   |   |   |   |   |   | \n");
+	printf("   |   |   |   |   |   | \n");
+	printf("\n");
+
+	printf("  \\|/ \\|/ \\|/ \\|/ \\|/ \\|/ \n");
+	printf("  - - - - - - - - - - - -  \n");
+    printf("   |   |   |   |   |   | \n");
+	printf("   |   |   |   |   |   | \n");
+	printf("\n"); 
+	break;
+    }
+    else
+    printf("你输入的密码不对哦,再给你一次机会，不然你女朋友是我的了\n");
+   }
+	return 0;
+}
